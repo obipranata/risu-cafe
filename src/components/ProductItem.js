c83236ee76c3520c92f5d4d45af55c157fe8a464
obipranata <obiimanuelpranata@gmail.com>
2023-07-13 16:22:24 +0700

    change condition

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -5,10 +5,16 @@ const ProductItem = ({menu, product, setModalProduct, setIsOpen}) => {
     className="product-card h-[211px] w-[155px] rounded-lg mt-4 relative border border-[#46E300] cursor-pointer product-item-border"
   >
       <div onClick={() => {setModalProduct(product); setIsOpen(true);}}>
-        <div className={`absolute top-1 right-1 ${menu == 'Cemilan' ? 'd-none' : ''}`}>
-          <p className={`text-[#AFE894] font-bold leading-4 text-xs ${!product.ice ? 'd-none' : ''}`} >ICE</p>
-          <p className={`text-[#AFE894] font-bold leading-4 text-xs ${!product.hot ? 'd-none' : ''}`} >HOT</p>
-        </div>
+        {
+          menu == 'Minuman' ? 
+          <div className="absolute top-1 right-1">
+            {product.ice ? <p className="text-[#AFE894] font-bold leading-4 text-xs" >ICE</p> : ''}
+            {product.hot ? <p className="text-[#AFE894] font-bold leading-4 text-xs" >HOT</p> : ''}
+          </div>
+          :
+          ''
+        }
+
         <img src={imgUrl+product.img} className="rounded-lg object-cover h-[211px] w-[155px]" />
         <div className="absolute bottom-0 px-4 pb-4">
           <h3 className="product-title text-primary leading-4 font-semibold text-base product-title-shadow" >
